d4522e6f25053dbe989d5ad9e5fc5cb0316a6514
Aaron <aaron@aaron-1015PEM.(none)>
2013-07-09 20:16:04 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+
+var index_html = fs.readFileSync('index.html').toString();
+
+console.log(index_html);
  
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index_html);
 });
 
 var port = process.env.PORT || 5000;
